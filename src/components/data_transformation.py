82bc786d00df93bc1b8bed82fe57e9d94930e15b
Shazeel-998 <shazeel.sadaqat@outlook.com>
2026-02-12 22:54:57 +0000

    predict pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join(BASE_DIR, 'artifacts', "proprocessor.pkl")
+    preprocessor_obj_file_path = os.path.join(BASE_DIR, 'artifacts', "preprocessor.pkl")
 
 
 class DataTransformation:
